0acf65c8372a5225a0c9f0b4b4732b222c8fb091
Muhammad Hafiz <108406631+MHafizz@users.noreply.github.com>
2024-01-06 10:23:02 +0700

    First Push

---
+++ app/src/main/java/com/example/praktik11/HalamanLinear.kt
@@ -0,0 +1,11 @@
+package com.example.praktik11
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HalamanLinear : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_halaman_linear)
+    }
+}
